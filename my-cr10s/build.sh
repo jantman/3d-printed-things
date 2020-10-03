7eb14fc0e896abcaeff69a0da9b6ff7b1a22cd4e
Jason Antman <jason@jasonantman.com>
2020-10-03 14:38:03 -0400

    finish printer STL builds

--- my-cr10s/build.sh
+++ my-cr10s/build.sh
@@ -3,9 +3,12 @@
 set -o errexit
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-time openscad -o output/printer.stl -D'show_printer=true' -D'show_control_box=false' CR-10.STEP.scad
+[[ -e output/printer.stl ]] || time openscad -o output/printer.stl -D'show_printer=true' -D'show_control_box=false' CR-10.STEP.scad
 # Note - output/printer.stl is 162MB. It's currently gitignored.
-admesh --write-binary-stl=output/printer_fixed.stl output/printer.stl
+[[ -e output/printer_fixed.stl ]] || admesh --write-binary-stl=output/printer_fixed.stl output/printer.stl
+if [[ ! -e output/printer_fixed_meshlab_shell.stl ]]; then
+  meshlabserver -i output/printer_fixed.stl -o output/printer_fixed_shell.stl -s output/make_shell.mlx
+fi
 
-time openscad -o output/control_box.stl -D'show_printer=false' -D'show_control_box=true' CR-10.STEP.scad
-admesh --write-binary-stl=output/control_box_fixed.stl output/control_box.stl
+#time openscad -o output/control_box.stl -D'show_printer=false' -D'show_control_box=true' CR-10.STEP.scad
+#admesh --write-binary-stl=output/control_box_fixed.stl output/control_box.stl
