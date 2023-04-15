232e80dbfac61b86fd44790b16d588412a093972
Jason Antman <jason@jasonantman.com>
2023-04-15 16:02:22 -0400

    set NOZZLE_TO_PROBE_OFFSET

--- my-cr10s/marlin-configs/Configuration.h
+++ my-cr10s/marlin-configs/Configuration.h
@@ -1486,7 +1486,8 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -1.85 }
+// got these offsets from: https://www.th3dstudio.com/hc/guides/upgrades-printer-information/creality-bl-touch-probe-mount-offsets-metal-bracket/
+#define NOZZLE_TO_PROBE_OFFSET { -44, -9, 0}
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
