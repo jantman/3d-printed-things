37277293cdd29e24b93ea8f0ddd1da5ab32d1afe
Jason Antman <jason@jasonantman.com>
2023-05-02 13:24:50 -0400

    WIP trying to get temp sensor to work

--- my-cr10s/marlin-configs/Configuration.h
+++ my-cr10s/marlin-configs/Configuration.h
@@ -100,7 +100,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT 2
+#define SERIAL_PORT -1
 
 /**
  * Serial Port Baud Rate
@@ -122,7 +122,7 @@
  * Currently Ethernet (-2) is only supported on Teensy 4.1 boards.
  * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT_2 -1
+//#define SERIAL_PORT_2 2
 //#define BAUDRATE_2 250000   // :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000] Enable to override BAUDRATE
 
 /**
@@ -539,6 +539,7 @@
  *
  */
 #define TEMP_SENSOR_0 -5
+#undef PS_ON_PIN
 #define TEMP_0_CS_PIN PC13
 // this is for DEBUGGING ONLY to try and figure out what's going on
 #define BOGUS_TEMPERATURE_GRACE_PERIOD 0
@@ -2480,7 +2481,7 @@
  * SD Card support is disabled by default. If your controller has an SD slot,
  * you must uncomment the following option or it won't work.
  */
-#define SDSUPPORT
+//#define SDSUPPORT
 
 /**
  * SD CARD: ENABLE CRC
