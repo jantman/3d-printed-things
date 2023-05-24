a9ad5f0180529467cf4d8af48e0addc0832a6e8d
Jason Antman <jason@jasonantman.com>
2023-05-24 16:09:28 -0400

    extruder steps per unit

--- my-cr10s/marlin-configs/Configuration.h
+++ my-cr10s/marlin-configs/Configuration.h
@@ -1175,7 +1175,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 139.78 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
